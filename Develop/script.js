74c29d7fe7e7e70b42edb649e3a7737bdb9a22bb
sselvag <swaathy.selva@gmail.com>
2022-06-22 22:17:05 -0400

    current day display working

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+
+// 1. WHEN I open the planner, THEN the current day is displayed at the top of the calendar
+var currentDay = $("#currentDay");
+currentDay.text(moment().format('dddd') + ", " + moment().format('MMMM Do'));
+
+
+// 2. WHEN I scroll down, THEN I am presented with time blocks for standard business hours
+// 3. WHEN I view the time blocks for that day, THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// 4. WHEN I click into a time block, THEN I can enter an event
+// 5. WHEN I click the save button for that time block, THEN the text for that event is saved in local storage
+// 6. WHEN I refresh the page, THEN the saved events persist
